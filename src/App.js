f098e3e800507b8d46fe5edd90329d3b7c221ca0
Bob Wise <bobwise@quickenloans.com>
2020-09-18 15:55:45 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { SprkStack, SprkStackItem, SprkSelectionInput, SprkCenteredColumn, SprkButton } from '@sparkdesignsystem/spark-react';
+import uniqueId from 'lodash/uniqueId';
+import IconSet from './IconSet';
 
 function App() {
+
+  const [rolledDice, setRolledDice] = useState([]);
+  const [newDiceType, setNewDiceType] = useState(6);
+  const [total, setTotal] = useState(0);
+
+  function addNewDice(dice) {
+    dice.value = rollDice(dice.maxValue);
+    setRolledDice(rolledDice.concat(dice));
+    const newTotal = total + dice.value;
+    setTotal(newTotal);
+  }
+
+  function rollDice(maxValue) {
+    const value = Math.floor(Math.random() * maxValue) + 1;
+    return value;
+  }
+
+  function clear() {
+    setRolledDice([]);
+    setTotal(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SprkCenteredColumn additionalClasses="app sprk-u-phh sprk-u-mvl">
+      <IconSet></IconSet>
+      <SprkSelectionInput
+        choices={[
+          { label: '4', value: '4' },
+          { label: '6', value: '6' },
+          { label: '8', value: '8' },
+          { label: '10', value: '10' },
+          { label: '12', value: '12' },
+          { label: '20', value: '20' },
+        ]}
+        variant="select"
+        label="Dice Type"
+        value={newDiceType}
+        onChange={(e) => setNewDiceType(e.target.value)}
+        additionalClasses="sprk-u-mbm"
+      />
+      <SprkStack itemSpacing="medium" splitAt="tiny" additionalClasses="sprk-u-mbm">
+        <SprkStackItem additionalClasses="sprk-o-Stack__item--half@xs ">
+          <SprkButton onClick={() => { addNewDice({ maxValue: newDiceType, id: uniqueId() }) }}>
+            Roll a dice
+          </SprkButton>
+        </SprkStackItem>
+        <SprkStackItem additionalClasses="sprk-o-Stack__item--half@xs">
+          <SprkButton variant="secondary" onClick={() => { clear() }}>
+            Start Over
+          </SprkButton>
+        </SprkStackItem>
+      </SprkStack>
+      <div className="sprk-u-mbm">
+        Total: {total}
+      </div>
+      <div className="rolledDiceContainer">
+        {rolledDice.map((dice) =>
+          <div className={"sprk-u-AbsoluteCenter sprk-u-mbm dice d" + dice.maxValue} key={dice.id}>
+            {dice.value}
+          </div>
+        )}
+      </div>
+    </SprkCenteredColumn>
   );
 }
 
