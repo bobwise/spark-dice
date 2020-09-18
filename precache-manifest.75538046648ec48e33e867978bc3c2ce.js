4a3456c1133b3332c7cb162ce4bd2a394538e98c
Bob Wise <bobwise@quickenloans.com>
2020-09-18 15:59:48 -0400

    Updates

---
+++ precache-manifest.75538046648ec48e33e867978bc3c2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a2e59ef6f0b55249ee3ce0c016ae8c",
+    "url": "/spark-dice/index.html"
+  },
+  {
+    "revision": "7398ae1e87200f269de5",
+    "url": "/spark-dice/static/css/main.628504ae.chunk.css"
+  },
+  {
+    "revision": "035a1824bd10d6b391ed",
+    "url": "/spark-dice/static/js/2.f155c00a.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/spark-dice/static/js/2.f155c00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7398ae1e87200f269de5",
+    "url": "/spark-dice/static/js/main.b51fbba4.chunk.js"
+  },
+  {
+    "revision": "ff9d601fe9413f3a1a6c",
+    "url": "/spark-dice/static/js/runtime-main.895ffa8b.js"
+  }
+]);
